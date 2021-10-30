31edb1dd7e4c210b99f1188e2971884743b70b3a
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-10-30 09:24:36 +0000

    added styling to menu, added products and product details functionally.

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -28,4 +28,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-        

281f0ddc2a572c955d564d4843a833e8bf01a260
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-11-10 13:35:03 +0000

    added quantity input and shopping bag functionality

--- bag/views.py
+++ bag/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
-def add_to_bag(request, item_id):   
+
+def add_to_bag(request, item_id):
     """add quantiy of the specified product to the shopping bag"""
 
     quantity = int(request.POST.get('quantity'))
@@ -17,9 +19,8 @@ def add_to_bag(request, item_id):
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
-        bag[item_id] = quantity    
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+
     return redirect(redirect_url)
-    

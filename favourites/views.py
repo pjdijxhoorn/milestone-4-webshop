526968314277c144934adb2f23d60c5d5886f40f
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-12-04 14:09:12 +0000

    added favourites app setup

--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def favourites(request):
+
+    """ displays the products listed as favourites by the user"""
+
+    template = 'favourites/favourite'
+    context = {}
+
+    return render(request, template, context)

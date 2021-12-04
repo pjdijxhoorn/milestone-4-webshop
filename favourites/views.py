0c49b16c78fd5e748ca646ed19a813949cec66b6
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-12-04 15:12:41 +0000

    added styling to favourite icon

--- favourites/views.py
+++ favourites/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+from products.models import Product
+
 # Create your views here.
 
 def favourites(request):
 
     """ displays the products listed as favourites by the user"""
+
+    """ get recipe.id  get user  compare if recipe is in users recipe favourite list 
+
+        if icon is clicked toggle remove/add
+        return redirect
+    
+    """
 
     template = 'favourites/favourites'
     context = {}

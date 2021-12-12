4cefbdaa7e797412bac69d1fcceb6764a42ab54f
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-12-12 20:12:05 +0000

    test username placholder reviewform

--- rating/views.py
+++ rating/views.py
@@ -12,7 +12,7 @@ def rating(request, product_id):
     """ A view to show the product rating/ review form  """
     form = RatingForm(request.POST)
     form.instance.name = request.user.username
-    product = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, product_id=product_id)
     form.instance.product = product.name
 
     if request.method == 'POST':

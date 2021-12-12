d081b105d2b135155673e6617c505419a1374234
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-12-12 16:31:08 +0000

    redo urls ratings

--- rating/views.py
+++ rating/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from products.models import Product
+
 
 
 from .forms import RatingForm
